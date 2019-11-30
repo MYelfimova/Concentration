71654b877b8ca9f6a15df2e795ab71c23b98dc74
MYelfimova <maria.yelfimova@gmail.com>
2019-11-30 19:13:09 +0200

    Initial commit.
    Card array created, a function that flips cards and a function that changes their colors created.

---
+++ Concentration.swift
@@ -0,0 +1,19 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Air on 30.11.2019.
+//  Copyright © 2019 Hummus Inc. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    
+    var cards = Array<Card>
+    
+    func chooseCard(at index: Int) {
+        
+    }
+    
+}
